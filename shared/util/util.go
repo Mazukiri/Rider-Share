76bb6705f4fcd0fbb249165a7aed1a7fe400cefc
CongLe <lequycong204@gmail.com>
2025-12-20 17:54:39 +0700

    Fix the URL error

--- shared/util/util.go
+++ shared/util/util.go
@@ -2,7 +2,7 @@ package util
 
 import "fmt"
 
-// GetRandomAvatar returns a random avatar URL from the randomuser.com API
+// GetRandomAvatar returns a random avatar URL from the randomuser.me API
 func GetRandomAvatar(index int) string {
-	return fmt.Sprintf("https://randomuser.com/api/portraits/lego/%d.jpg", index)
+	return fmt.Sprintf("https://randomuser.me/api/portraits/lego/%d.jpg", index)
 }
