acf4fa9732b4f02b6b1c3fe22cd21f1f5692f324
test <tran.hoang.dat.2312@gmail.com>
2025-12-20 18:03:06 +0700

    Generate template creation for future microservices

---
+++ tools/create_service.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	serviceName := flag.String("name", "", "Name of the service (e.g., user, payment)")
+	flag.Parse()
+
+	if *serviceName == "" {
+		fmt.Println("Please provide a service name using -name flag")
+		os.Exit(1)
+	}
+
+	// Create service directory structure
+	basePath := filepath.Join("services", *serviceName+"-service")
+	dirs := []string{
+		"cmd",
+		"internal/domain",
+		"internal/service",
+		"internal/infrastructure/events",
+		"internal/infrastructure/grpc",
+		"internal/infrastructure/repository",
+		"pkg/types",
+	}
+
+	for _, dir := range dirs {
+		fullPath := filepath.Join(basePath, dir)
+		if err := os.MkdirAll(fullPath, 0755); err != nil {
+			fmt.Printf("Error creating directory %s: %v\n", dir, err)
+			os.Exit(1)
+		}
+	}
+
+	// Create an empty README.md
+	readmePath := filepath.Join(basePath, "README.md")
+}
