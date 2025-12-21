e26090a9fda6c51f3f671a6fd02d51266fb9ade4
test <tran.hoang.dat.2312@gmail.com>
2025-12-21 15:44:46 +0700

    Import database queries

--- services/trip-service/internal/infrastructure/repository/mongodb.go
+++ services/trip-service/internal/infrastructure/repository/mongodb.go
@@ -20,3 +20,58 @@ type mongoRepository struct {
 func NewMongoRepository(db *mongo.Database) *mongoRepository {
 	return &mongoRepository{db: db}
 }
+
+func (r *mongoRepository) CreateTrip(ctx context.Context, trip *domain.TripModel) (*domain.TripModel, error) {
+	result, err := r.db.Collection(db.TripsCollection).InsertOne(ctx, trip)
+	if err != nil {
+		return nil, err
+	}
+
+	trip.ID = result.InsertedID.(primitive.ObjectID)
+
+	return trip, nil
+}
+
+func (r *mongoRepository) GetTripByID(ctx context.Context, id string) (*domain.TripModel, error) {
+	_id, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	result := r.db.Collection(db.TripsCollection).FindOne(ctx, bson.M{"_id": _id})
+	if result.Err() != nil {
+		return nil, result.Err()
+	}
+
+	var trip domain.TripModel
+	err = result.Decode(&trip)
+	if err != nil {
+		return nil, err
+	}
+
+	return &trip, nil
+}
+
+func (r *mongoRepository) UpdateTrip(ctx context.Context, tripID string, status string, driver *pbd.Driver) error {
+	_id, err := primitive.ObjectIDFromHex(tripID)
+	if err != nil {
+		return err
+	}
+
+	update := bson.M{"$set": bson.M{"status": status}}
+
+	if driver != nil {
+		update["$set"].(bson.M)["driver"] = driver
+	}
+
+	result, err := r.db.Collection(db.TripsCollection).UpdateOne(ctx, bson.M{"_id": _id}, update)
+	if err != nil {
+		return err
+	}
+
+	if result.ModifiedCount == 0 {
+		return fmt.Errorf("trip not found: %s", tripID)
+	}
+
+	return nil
+}
